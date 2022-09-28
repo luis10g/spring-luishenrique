130a2f393d9de9abc4861b36267e01bf17a20fde
luis10g <joicefernandasouzaoliveira@gmail.com>
2022-09-28 18:01:57 +0000

    Prova

---
+++ app/src/main/java/luishenrique/controllers/AlunosController.java
@@ -0,0 +1,37 @@
+package luishenrique.controllers;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.stereotype.Controller;
+ import org.springframework.ui.Model;
+ import org.springframework.web.bind.annotation.RequestMapping;
+ import org.springframework.web.bind.annotation.RequestMethod;
+ import org.springframework.web.bind.annotation.RequestParam;
+
+ import luishenrique.models.Aluno;
+ import luishenrique.repositories.AlunosRepository;
+
+@Controller
+@RequestMapping("/alunos") 
+public class AlunosController {  
+    @Autowired
+    private AlunosRepository alunosRepo;
+
+        @RequestMapping("list") 
+         public String list(Model model) {
+             model.addAttribute("alunos", this.alunosRepo.findAll());
+              return"list";
+    } 
+
+        @RequestMapping("insert")
+         public String insert() { 
+            return"insert";     
+     }  
+
+        @RequestMapping(value = "insert", method = RequestMethod.POST) 
+        public String insert(@RequestParam("nome") String nome, @RequestParam("idade") int idade) {
+            Aluno aluno = newAluno();
+            aluno.setNome(nome);
+            aluno.setIdade(idade);
+            alunosRepo.save(aluno);
+            return"redirect:/alunos/list";     
+            } 
+        } 
---
+++ app/src/main/java/luishenrique/repositories/AlunosRepository.java
@@ -0,0 +1,9 @@
+package luishenrique.repositories;
+
+import org.springframework.data.repository.CrudRepository; 
+
+import luishenrique.models.Aluno;
+
+public interface AlunosRepository extendsCrudRepository<Aluno, Integer> { 
+
+} 
